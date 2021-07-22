d6eac9ba8d2e269ed9a4733c1702d79cb6466ddf
anthonydoes <anthonydoesdev@gmail.com>
2021-07-22 19:32:40 -0400

    added jquery back

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-// jquery CDN
-var jQueryScript = document.createElement('script');  
-jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-document.head.appendChild(jQueryScript);
+
 
 
 // Typewriter effect
