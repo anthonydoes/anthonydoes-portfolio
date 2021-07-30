a69645b9d838a322a2584a9610f9d29a9a94f5f1
anthonydoes <anthonypyannos@gmail.com>
2021-07-29 21:16:58 -0400

    Added JS for scroll events to modify background color

--- main.js
+++ main.js
@@ -114,6 +114,47 @@ $('.filters-list a').on('click', function() {
 }) 
 
 
+
+// Page scroll color effects
+
+
+$(document).on('scroll', function() {
+  var pixelsFromTop = $(document).scrollTop()
+
+  if (pixelsFromTop > 50) {
+    $('header').addClass('hidden')
+  } else {
+    $('header').removeClass('hidden')
+  }
+
+  if (pixelsFromTop < 600) {
+    $('body').css('background-color', '#091a33ff')
+  } else if (pixelsFromTop < 1400) {
+    $('body').css('background-color', 'rgb(33 65 112)')
+  } else if (pixelsFromTop < 2200) {
+    $('body').css('background-color', '#091a33ff')
+  } else if (pixelsFromTop < 3000) {
+    $('body').css('background-color', '#000000')
+  } else {
+    $('body').css('background-color', '#091a33ff')
+  }
+
+
+  var documentHeight = $(document).height()
+  var windowHeight = $(window).height()
+
+  var difference = documentHeight - windowHeight
+
+  var percentage = 100 * pixelsFromTop / difference
+
+  console.log(percentage)
+  
+  $('.bar').css('width', percentage + '%')
+  
+})
+
+
+
                       
                       
                      
